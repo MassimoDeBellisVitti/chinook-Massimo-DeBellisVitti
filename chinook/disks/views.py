399e19f9117e3bdb6e33aa2247f0b4af8edb1391
Massimo De Bellis Vitti <debellisvitti.massimo@gmail.com>
2025-03-31 15:33:16 +0200

    album list created

--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Album
 
-# Create your views here.
+def album_list(request):
+    albums = Album.objects.all()
+    return render(request, 'album_list.html', {'albums': albums})
