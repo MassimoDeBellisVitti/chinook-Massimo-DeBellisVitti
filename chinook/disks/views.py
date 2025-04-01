d629de00628e43c23765df7863db37b6000fd573
Massimo De Bellis Vitti <debellisvitti.massimo@gmail.com>
2025-04-01 17:45:08 +0200

    artist research added

--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Album
+from django.db.models import Q
 
 def album_list(request):
     query = request.GET.get('q')  # prende il testo inserito nel campo di ricerca
     if query:
-        albums = Album.objects.filter(title__icontains=query)
+        albums = Album.objects.filter( Q(title__icontains=query) | Q(artist__name__icontains=query))
     else:
         albums = Album.objects.all()
     return render(request, 'album_list.html', {
